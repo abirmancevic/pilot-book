dde0e9c64f3c05699bf17c1c4ef42272c0407516
abirmancevic <157796498+abirmancevic@users.noreply.github.com>
2024-05-07 21:04:55 +0200

    Update and rename functionOriginal.m to functionBase.m

--- Zadoff-Chu/functionBase.m
+++ Zadoff-Chu/functionBase.m
@@ -1,8 +1,8 @@
-function S = functionOriginal(N_zc, qOrig)
+function S = functionBase(N_zc, qBase)
 
 %% Description of the Input parameters
-% N_zc:   1 x 1; a desired length of original sequences
-% qOrig:  (N_zc - 1) x 1 or 1 x (N_zc  - 1); desired root indices
+% N_zc:   1 x 1; a desired length of base sequences
+% qBase:  (N_zc - 1) x 1 or 1 x (N_zc  - 1); desired root indices
 %
 %   Prerequisites:
 % N_zc >= 3
@@ -10,22 +10,22 @@ function S = functionOriginal(N_zc, qOrig)
 % ~ Even numbers are not considered for N_zc.
 
 %% Description of the Output parameters
-% S:   N_zc x length(qOrig); created original sequences
+% S:   N_zc x length(qBase); created base sequences
 
 %% Standalone usage (uncomment lines 17 - 20 and 36 - 52)
 %   Part I
 %      arguments
 %  N_zc (1,1) {mustBeInteger,mustBeGreaterThanOrEqual(N_zc,3), mustBeOdd}
-%  qOrig (1,:) {mustBeInteger,mustBeInRange(qOrig,1,N_zc,"exclude-upper"),mustBeUnique}
+%  qBase (1,:) {mustBeInteger,mustBeInRange(qBase,1,N_zc,"exclude-upper"),mustBeUnique}
 %      end
 
-%% Generating the original base sequence, s_q of the length N_zc
+%% Generating the base sequence, s_q of the length N_zc
 s_q = zeros(N_zc, 1);
-S = zeros(N_zc, length(qOrig));
+S = zeros(N_zc, length(qBase));
 
-for i = 1 : length(qOrig)
+for i = 1 : length(qBase)
     for n = 0 : N_zc - 1
-        s_q(n + 1) = exp( -1i * pi * qOrig(i) * (n * (n + 1) ) / N_zc ); % a base sequence
+        s_q(n + 1) = exp( -1i * pi * qBase(i) * (n * (n + 1) ) / N_zc ); % a created base sequence
     end
     S(:,i) = s_q;
 end
