2242d4fe703abc663b36bb4db970366f86c54a25
abirmancevic <157796498+abirmancevic@users.noreply.github.com>
2024-05-07 20:56:15 +0200

    Update functionCyclicShifting.m

--- Zadoff-Chu/functionCyclicShifting.m
+++ Zadoff-Chu/functionCyclicShifting.m
@@ -5,25 +5,26 @@ function XCyclicShifted = functionCyclicShifting(X)
 % The zero cyclic shift is not considered (i.e. m = 0, see line 32), which
 % may be also named as a quasi cyclic shift.
 % The function is intended for use with one of the following functions: 
-% "functionOriginal", "functionCyclicExtending" or "functionTruncation".
+% "functionBase", "functionCyclicExtending" or "functionTruncation".
 
 %% Description of the Input parameters
-% X:   N_zc x length(qOrig) or N_sf x length(qCycExt)
+% X:   N_zc x length(qBase) or N_sf x length(qCycExt)
 %      or N_sf x length(qTrun);
 
 %% Description of the Output parameters
-% XCyclicShifted:   length(qOrig) x N_zc x (N_zc - 1),
+% XCyclicShifted:   length(qBase) x N_zc x (N_zc - 1),
 %                   length(qCycExt) x NCycExt_sf x (NCycExt_sf - 1),
 %                   or length(qTrun) x NTrun_sf x (NTrun_sf - 1);
-%   if ( length(qOrig) > 1 ).
+%   if ( length(qBase) > 1 ).
 %
 % XCyclicShifted:   N_zc x (N_zc - 1),
 %                   NCycExt_sf x (NCycExt_sf - 1),
 %                   or NTrun_sf x (NTrun_sf - 1);
 %   otherwise.
 
-%% Generating N - 1 cyclic shifts of original, cyclic extended or truncated
-% sequence of the length N (= N_zc, NCycExt_sf, or NTrun_sf, respectively).
+%% Generating N - 1 cyclic shifts of the initial sequence:
+% i.e. either a base, cyclic extended or truncated sequence of the length 
+% N (= N_zc, NCycExt_sf, or NTrun_sf, respectively).
 N = size(X, 1);
 nbrOfq = size(X, 2);
 XCyclicShifted = zeros(nbrOfq, N, N - 1);
